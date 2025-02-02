05ead3d8431a9fbb2b609b031e36e2e38a48b1cf
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2025-02-02 23:40:34 +0530

    fetch data from login API and add credential to aviod cors error

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,8 +1,65 @@
+import { useState } from "react";
+import axios from "axios";
 
 const LogIn = () => {
-  return (
-    <div>LogIn</div>
-  )
-}
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LogIn
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                placeholder=""
+                value={emailId}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="password"
+                value={password}
+                placeholder=""
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Log In
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
